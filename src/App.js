aed0a4dd32846a5195f93bfd4778bd71454374d9
Meagan Louise <meaganledwards@gmail.com>
2022-01-11 18:07:58 +0000

    Added css
    Added css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header">Dictionary</header>
         <main>
-          <Dictionary defaultKeyword="hello" />
+          <Dictionary defaultKeyword="watermelon" />
         </main>
         <footer className="App-footer">
           Coded by{" "}
