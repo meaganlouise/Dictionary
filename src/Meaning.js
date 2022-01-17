20fcc8f18d796ccc960b4ad652e05c09ad1f0ea4
Meagan Louise <meaganledwards@gmail.com>
2022-01-17 21:44:13 +0000

    Changed default keyword

--- src/Meaning.js
+++ src/Meaning.js
@@ -11,7 +11,7 @@ export default function Meaning(props) {
           <div key={index}>
             <div className="definition">{definition.definition}</div>
 
-            <div className="example">"{definition.example}" </div>
+            <div className="example">{definition.example} </div>
             <Synonyms synonyms={definition.synonyms} />
           </div>
         );
